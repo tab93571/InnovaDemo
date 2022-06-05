ffa67ffe500b88bcb25ad10e648b4d61d001d1c8
austin0502 <99103036austin@gmail.com>
2022-06-05 14:20:12 +0800

    commit

--- src/test/java/com/integration/ValidationControllerIntegrationTest.java
+++ src/test/java/com/integration/ValidationControllerIntegrationTest.java
@@ -4,12 +4,16 @@ import com.DemoApplication;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.request.AddValidateServiceRequest;
+import com.request.DeleteValidateServiceRequest;
 import com.request.ValidateRequest;
 import com.response.Response;
+import com.service.ValidateService;
 import com.util.MockMvcUtil;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -18,9 +22,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-import static com.constant.MappingConstant.VALIDATE;
+import static com.constant.MappingConstant.*;
+import static com.constant.MappingConstant.ADDVALIDATESERVICE;
 import static com.enums.ResponseEnum.Fail;
 import static com.enums.ResponseEnum.Success;
 import static com.enums.ResponseEnum.*;
@@ -36,6 +44,8 @@ public class ValidationControllerIntegrationTest {
     private ObjectMapper mapper;
     @Autowired
     private MockMvcUtil mockMvcUtil;
+    @Autowired
+    private Set<ValidateService> validationSet;
 
     //------------------ validate------------------------------
 
@@ -61,8 +71,12 @@ public class ValidationControllerIntegrationTest {
         Response<List<Response>> response = mapper.readValue(returnString,reference);
 
         Assert.assertEquals(Fail.getCode(),response.getCode());
-        Assert.assertEquals(1,response.getData().size());
-        Assert.assertEquals(ValidationPasswordNullOrEmptyError.getCode(),response.getData().get(0).getCode());
+
+        List<Integer> actualResponseListCode = response.getData().stream().map(res ->  res.getCode()).collect(Collectors.toList());
+        List<Integer> expectResponseListCode = Arrays.asList(ValidationPasswordNullOrEmptyError.getCode());
+
+        Assert.assertEquals(expectResponseListCode.size(),actualResponseListCode.size());
+        Assert.assertTrue(actualResponseListCode.containsAll(expectResponseListCode));
     }
 
     /**
@@ -84,22 +98,127 @@ public class ValidationControllerIntegrationTest {
         Response<List<Response>> response = mapper.readValue(returnString,reference);
 
         Assert.assertEquals(Fail.getCode(),response.getCode());
-        Assert.assertEquals(1,response.getData().size());
-        Assert.assertEquals(ValidationPasswordNullOrEmptyError.getCode(),response.getData().get(0).getCode());
+
+        List<Integer> actualResponseListCode = response.getData().stream().map(res ->  res.getCode()).collect(Collectors.toList());
+        List<Integer> expectResponseListCode = Arrays.asList(ValidationPasswordLengthError.getCode(),ValidationPasswordNoLowerCaseLetters.getCode(),ValidationPasswordNoNumericalDigits.getCode(),
+                ValidationPasswordCharacterNotAllowed.getCode(),ValidationSequenceOfCharactersFollowedByTheSameSequence.getCode());
+
+        Assert.assertEquals(expectResponseListCode.size(),actualResponseListCode.size());
+        Assert.assertTrue(actualResponseListCode.containsAll(expectResponseListCode));
     }
 
+    //------------------ get service------------------------------
+
+    @Test
+    public void testGetValidateServiceNameInUse() throws Exception {
+
+        String returnString = mockMvcUtil.getMethod(GETVALIDATESERVICESINUSE);
 
 
+        Response response = mapper.readValue(returnString,Response.class);
 
+        List<String>responseDataActual = (List<String>)response.getData();
+        Set<String>responseDataExpected = validationSet.stream().map(service -> service.getName()).collect(Collectors.toSet());
 
-    //------------------ get------------------------------
+        Assert.assertEquals(Success.getCode(),response.getCode());
 
+        Assert.assertEquals(responseDataExpected.size(),responseDataActual.size());
+        Assert.assertTrue(responseDataActual.containsAll(responseDataExpected));
 
+    }
 
+    //------------------ delete service------------------------------
 
-    //------------------ add------------------------------
+    /**
+     * bean exists in the validationSet
+     */
+    @Test
+    public void testDeleteValidateServiceInUseSuccess() throws Exception {
 
+        DeleteValidateServiceRequest request = new DeleteValidateServiceRequest("validatePasswordCheckRepeatedSequenceService");
 
+        TypeReference reference = new TypeReference<Response<List<String>>>() {};
 
-    //------------------ delete------------------------------
+        String returnStringGet = mockMvcUtil.getMethod(GETVALIDATESERVICESINUSE);
+
+        Response<List<String>> responseGet = mapper.readValue(returnStringGet,reference);
+
+        int originServiceSize = responseGet.getData().size();
+
+        String returnString = mockMvcUtil.deleteMethod(DELETEVALIDATESERVICEINUSE,request);
+
+        Response response = mapper.readValue(returnString,Response.class);
+
+        Assert.assertEquals(Success.getCode(),response.getCode());
+
+        String returnStringGetAfterDelete = mockMvcUtil.getMethod(GETVALIDATESERVICESINUSE);
+
+        Response<List<String>> responseGetAfterDelete = mapper.readValue(returnStringGetAfterDelete,reference);
+
+        int afterDeleteServiceSize = responseGetAfterDelete.getData().size();
+
+        Assert.assertTrue(afterDeleteServiceSize == originServiceSize - 1);
+    }
+
+    /**
+     * bean doesn't exist in the validationSet
+     */
+    @Test
+    public void testDeleteValidateServiceInUseNotFound() throws Exception {
+
+        DeleteValidateServiceRequest request = new DeleteValidateServiceRequest("");
+
+        TypeReference reference = new TypeReference<Response<List<String>>>() {};
+
+        String returnStringGet = mockMvcUtil.getMethod(GETVALIDATESERVICESINUSE);
+
+        Response<List<String>> responseGet = mapper.readValue(returnStringGet,reference);
+
+        int originServiceSize = responseGet.getData().size();
+
+        String returnString = mockMvcUtil.deleteMethod(DELETEVALIDATESERVICEINUSE,request);
+
+        Response response = mapper.readValue(returnString,Response.class);
+
+        Assert.assertEquals(ValidationCanNotFindService.getCode(),response.getCode());
+
+        String returnStringGetAfterDelete = mockMvcUtil.getMethod(GETVALIDATESERVICESINUSE);
+
+        Response<List<String>> responseGetAfterDelete = mapper.readValue(returnStringGetAfterDelete,reference);
+
+        int afterDeleteServiceSize = responseGetAfterDelete.getData().size();
+
+        Assert.assertTrue(afterDeleteServiceSize == originServiceSize );
+    }
+
+    //------------------ add service------------------------------
+
+    /**
+     * bean doesn't exist in the spring container
+     */
+    @Test
+    public void testAddValidateServiceInUseNotExistInSpringContainer() throws Exception {
+
+        AddValidateServiceRequest request = new AddValidateServiceRequest("");
+
+        String returnString = mockMvcUtil.postMethod(ADDVALIDATESERVICE,request);
+
+        Response response = mapper.readValue(returnString,Response.class);
+        Assert.assertEquals(ValidationCanNotFindService.getCode(),response.getCode());
+
+    }
+
+    /**
+     * the type of bean isn't matched
+     */
+    @Test
+    public void testAddValidateServiceBeanTypeNotMatched() throws Exception {
+
+        AddValidateServiceRequest request = new AddValidateServiceRequest("generalValidateServiceImpl");
+
+        String returnString = mockMvcUtil.postMethod(ADDVALIDATESERVICE,request);
+
+        Response response = mapper.readValue(returnString,Response.class);
+        Assert.assertEquals(ValidationServiceTypeNotMatch.getCode(),response.getCode());
+    }
 }
