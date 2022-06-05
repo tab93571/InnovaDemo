e2f54894827f8907311e90507d0399b6f1c1c522
austin0502 <99103036austin@gmail.com>
2022-06-05 15:17:41 +0800

    commit

--- src/main/java/com/controller/ValidationController.java
+++ src/main/java/com/controller/ValidationController.java
@@ -9,6 +9,7 @@ import com.request.ValidateRequest;
 import com.response.Response;
 import com.service.GeneralValidateService;
 import com.util.ResponseUtil;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,12 +35,15 @@ public class ValidationController {
             return ResponseUtil.error(ResponseEnum.Fail,responseList);
         }
     }
-
     @GetMapping(GETVALIDATESERVICESINUSE)
     public Response getValidateServicesInUse(){
         return service.getValidateServiceNameInUse();
     }
-
+    @ApiOperation(value = "delete the validation service we are using",notes =
+            "change which validation logic in runtime" +
+            "but if there are multiple pods we need to use redis channel to reach to every pods(send to the service and it publishes and other pods subscribe that channel and operate"+
+            "but when the service restart all the operation will be lost"+
+            "or we can store a validationService list in redis and we modify that list in redis, every time we run validate method we can check the verificationList in redis and decide if we need to operate that")
     @DeleteMapping(DELETEVALIDATESERVICEINUSE)
     public Response deleteValidateServiceInUse(@RequestBody DeleteValidateServiceRequest req){
         return service.deleteValidateServiceInUse(req);
