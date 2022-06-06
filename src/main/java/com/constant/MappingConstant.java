d38265be8f594e21fe03b410ab54ffe4b57db3c5
austin0502 <99103036austin@gmail.com>
2022-06-06 13:53:12 +0800

    add MappingConstant

---
+++ src/main/java/com/constant/MappingConstant.java
@@ -0,0 +1,8 @@
+package com.constant;
+
+public interface MappingConstant {
+    String VALIDATE = "/validate";
+    String GETVALIDATESERVICESINUSE = "/getValidateServicesInUse";
+    String DELETEVALIDATESERVICEINUSE = "/deleteValidateServiceInUse";
+    String ADDVALIDATESERVICE = "/addValidateService";
+}
